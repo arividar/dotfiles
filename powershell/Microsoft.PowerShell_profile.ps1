1fdab4689edcde7ec1cc57a60920ebc25353680f
Ari Johannesson <arividar@gmail.com>
2020-04-01 22:24:51 +0000

    Adds old profile commands - from 2014 I think

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,32 @@
-# Good info about profiles can be found here: https://bit.ly/2UXCBBK
+# Good info on Powershell profiles can be found here: https://bit.ly/2UXCBBK
 # Emacs edit mode like in bash
 
+Import-Module Pscx
+
+# Recover the bash utils:
+Remove-Item alias:ls
+Remove-Item alias:cat
+Remove-Item alias:tail	
+Remove-Item alias:man
+Remove-Item Function:less
+
+$NODE_PATH="C:\Users\arividar\AppData\Roaming\npm\node_modules"
+$POSHGIT_PATH="C:\tools\poshgit\dahlbyk-posh-git-3db1e6e"
+
+# Load posh-git example profile
+. "$POSHGIT_PATH\profile.example.ps1"
+
+function which
+{
+	param($command)
+	Get-Command $command | Select-Object -ExpandProperty Definition
+}
+
+function subl 
+{ 
+	&"${Env:ProgramFiles}\Sublime Text 3\sublime_text.exe" $args 
+}
+
 Set-PSReadLineOption -EditMode Emacs
+
+
