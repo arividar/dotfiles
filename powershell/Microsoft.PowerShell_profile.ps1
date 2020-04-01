33e26cbbfa088f10818f7fb574826330040831c5
Ari Johannesson <arividar@gmail.com>
2020-04-01 22:29:19 +0000

    Removes old stuff from profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -10,23 +10,10 @@ Remove-Item alias:tail
 Remove-Item alias:man
 Remove-Item Function:less
 
-$NODE_PATH="C:\Users\arividar\AppData\Roaming\npm\node_modules"
-$POSHGIT_PATH="C:\tools\poshgit\dahlbyk-posh-git-3db1e6e"
-
-# Load posh-git example profile
-. "$POSHGIT_PATH\profile.example.ps1"
-
 function which
 {
 	param($command)
 	Get-Command $command | Select-Object -ExpandProperty Definition
 }
 
-function subl 
-{ 
-	&"${Env:ProgramFiles}\Sublime Text 3\sublime_text.exe" $args 
-}
-
 Set-PSReadLineOption -EditMode Emacs
-
-
