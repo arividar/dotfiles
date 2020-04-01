5f409397a139ae8f82c7e2123d4ab395c643f2ac
Ari Johannesson <arividar@gmail.com>
2020-04-01 22:34:58 +0000

    simpler

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,5 @@
 # Good info on Powershell profiles can be found here: https://bit.ly/2UXCBBK
-
-# Recover the bash utils:
-Remove-Item alias:ls
-Remove-Item alias:cat
-Remove-Item alias:tail	
-Remove-Item alias:man
-Remove-Item Function:less
+# Emacs edit mode like in bash
 
 function which
 {
@@ -13,5 +7,4 @@ function which
 	Get-Command $command | Select-Object -ExpandProperty Definition
 }
 
-# Emacs edit mode like in bash
 Set-PSReadLineOption -EditMode Emacs
