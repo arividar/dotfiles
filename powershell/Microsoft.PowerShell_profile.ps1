e1791411279107525bccaf54a61d24566128522f
Ari Johannesson <arividar@gmail.com>
2020-04-12 12:15:50 +0000

    edit mode vi

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,10 @@
+Import-Module posh-git
+
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadlineOption -BellStyle None 
+
 function which
 {
 	param($command)
 	Get-Command $command | Select-Object -ExpandProperty Definition
 }
-
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadlineOption -BellStyle None 
-
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme tehrob
