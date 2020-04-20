ef41ed3fdda370d4eef4f81c8c36e0bea722a918
Ari Johannesson <arividar@gmail.com>
2020-04-20 18:27:09 +0000

    Adds isntall shell script for installing vim plugins

---
+++ vim/install_plugins.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/.vim/pack/ari/start
+
+# Polyglot
+git clone https://github.com/sheerun/vim-polyglot ~/.vim/pack/ari/start/vim-polyglot
+
+# NERDTree
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/ari/start/nerdtree
+vim -u NONE -c "helptags ~/.vim/pack/ari/start/nerdtree/doc" -c q
