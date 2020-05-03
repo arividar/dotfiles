13e8e70e5628fd807fd172651de82cde88cbe611
Ari Johannesson <arividar@gmail.com>
2020-05-03 07:30:55 +0000

    refactor

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,14 @@
 # Good info on Powershell profiles can be found here: https://bit.ly/2UXCBBK
 
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadlineOption -BellStyle None
+
+Import-Module oh-my-posh
+Import-Module posh-git
+
+Set-Theme Powerlevel10k-Lean
+
+# Functions and aliases:
 function which
 {
     param($command)
@@ -7,10 +16,3 @@ function which
 }
 Set-Alias l Get-ChildItem -Force
 
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadlineOption -BellStyle None
-
-Import-Module oh-my-posh
-Set-Theme Powerlevel10k-Lean
-
-Import-Module posh-git
