d396df458c2afc8b0c1abc8957a6ba67a252f0a2
Ari Johannesson <arividar@gmail.com>
2020-05-03 07:29:09 +0000

    adds l alias

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,16 @@
 # Good info on Powershell profiles can be found here: https://bit.ly/2UXCBBK
 
-Set-PSReadLineOption -EditMode Vi
-Set-PSReadlineOption -BellStyle None
-
-Import-Module oh-my-posh
-Set-Theme pure
-
-Import-Module posh-git
-
 function which
 {
     param($command)
     Get-Command $command | Select-Object -ExpandProperty Definition
 }
+Set-Alias l Get-ChildItem -Force
+
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadlineOption -BellStyle None
+
+Import-Module oh-my-posh
+Set-Theme Powerlevel10k-Lean
+
+Import-Module posh-git
