01afb61b7249616503655b7e2bed0641cb659991
Ari Johannesson <arividar@gmail.com>
2020-05-03 12:26:50 +0000

    Documents junction not needed

--- powershell/Goodies.ps1
+++ powershell/Goodies.ps1
@@ -1,5 +1,2 @@
 # Enable long paths (>260 chars) on Windows:
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name LongPathsEnabled -Type DWord -Value 1
-
-# Junction link ~\Documents folder to ~\OneDrive\Documents
-New-Item -ItemType Junction -Path C:\Users\Ari\Documents -Target C:\Users\Ari\OneDrive\Documents
