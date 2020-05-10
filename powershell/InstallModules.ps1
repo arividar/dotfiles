5a915874ad1ee469ccc7360a63330a37dc3a6b8f
Ari Johannesson <arividar@gmail.com>
2020-05-10 12:41:40 +0000

    Install module GraphicalTools to get Out-GridView

--- powershell/InstallModules.ps1
+++ powershell/InstallModules.ps1
@@ -1,5 +1,6 @@
 Install-Module posh-git -Scope CurrentUser
 Install-Module oh-my-posh -Scope CurrentUser
+Install-Module Microsoft.PowerShell.GraphicalTools -Scope CurrentUser
 
 # Install font Cascadia Mono Regular
 #    https://github.com/microsoft/cascadia-code
