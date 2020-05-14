7af5d19d7a373fa7e349b279b95b4300885db6b0
Ari Johannesson <arividar@gmail.com>
2020-05-14 20:14:21 +0000

    jsonc plugin

--- vim/install_vim_plugins.sh
+++ vim/install_vim_plugins.sh
@@ -4,6 +4,9 @@ mkdir -p ~/.vim/pack/ari/start
 # Polyglot
 git clone https://github.com/sheerun/vim-polyglot ~/.vim/pack/ari/start/vim-polyglot
 
+# jsonc highlighting (missing from polyglot)
+git clone https://github.com/kevinoid/vim-jsonc.git ~/.vim/pack/ari/start/vim-jsonc
+
 # NERDTree
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/ari/start/nerdtree
 vim -u NONE -c "helptags ~/.vim/pack/ari/start/nerdtree/doc" -c q
