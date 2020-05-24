f6dc571cf531e391c2b937205cf58ef867e46e30
Ari Johannesson <arividar@gmail.com>
2020-05-24 23:19:18 +0000

    more git config

--- github/config.sh
+++ github/config.sh
@@ -1,1 +1,4 @@
- git config --global core.editor "vim"
+git config --global user.name "Ari Johannesson"
+git config --global user.email "arividar@gmail.com"
+git config --global color.ui true
+git config --global core.editor "vim"
