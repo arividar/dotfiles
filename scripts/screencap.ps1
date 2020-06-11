cb45681b3b5c754a130bdeb01e289e51e61958a9
Ari Johannesson <arividar@gmail.com>
2020-06-11 14:57:13 +0000

    Set frequency and path for screencapture

--- scripts/screencap.ps1
+++ scripts/screencap.ps1
@@ -1,8 +1,11 @@
 #!/usr/local/bin/pwsh
 
+Set-Variable capPath -option Readonly -value '/Users/ari/OneDrive/TimeTrack/Capture' 
+Set-Variable capFrequency -option Readonly -value 300
+
+Write-Host "Capturing screenshots to $capPath every $capFrequency seconds"
 while($true) {
     $vardate = (& Invoke-Expression "date +%Y-%m-%dT%H.%M.%S" | Out-String) -replace "`n|`r"
-    & screencapture -t jpg -x "/Users/ari/OneDrive/TimeTrack/Capture/$vardate`.jpg"
-    Write-Host "/Users/ari/OneDrive/TimeTrack/Capture/$vardate`.jpg"
-    Start-Sleep -s 10
+    & screencapture -t jpg -x "$capPath/$vardate`.jpg"
+    Start-Sleep -s $capFrequency
 }
