6a288cc775582c4f69fde7e88475b44d8779f10a
Ari Johannesson <arividar@gmail.com>
2020-06-12 20:21:12 +0000

    Refactore capturescreen

--- scripts/capturescreen.ps1
+++ scripts/capturescreen.ps1
@@ -1,12 +1,16 @@
 #!/usr/local/bin/pwsh
 
-Add-Type -AssemblyName System.Windows.Forms
-Add-type -AssemblyName System.Drawing
+function CaptureScreen {
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
+    $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $bitmap = New-Object System.Drawing.Bitmap $screen.Width, $screen.Height
+    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphic.CopyFromScreen($screen.Left, $screen.Top, 0, 0, $bitmap.Size)
+    return $bitmap;
+}
 
-$screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-$bitmap = New-Object System.Drawing.Bitmap $screen.Width, $screen.Height
-$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-$graphic.CopyFromScreen($screen.Left, $screen.Top, 0, 0, $bitmap.Size)
-
+$bitmap = CaptureScreen
 $bitmap.Save("$PSScriptRoot\shot.jpg", [System.Drawing.Imaging.ImageFormat]::Jpeg)
 Write-Output "Screenshot saved to: $PSScriptRoot\shot.jpg"
+
