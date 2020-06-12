cf1d3cbd08066b26b4730399928a908a7db61f66
Ari Johannesson <arividar@gmail.com>
2020-06-12 21:56:32 +0000

    Add capscr.ps1 - screen capture on both mac and win

---
+++ scripts/capscr.ps1
@@ -0,0 +1,37 @@
+#!/usr/local/bin/pwsh
+
+Set-Variable capPath -option Readonly -value '/Users/ari/OneDrive/TimeTrack/Capture' 
+
+function WriteScreenshotWin([string]$screenShotPath) {
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
+    $screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $bitmap = New-Object System.Drawing.Bitmap $screen.Width, $screen.Height
+    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphic.CopyFromScreen($screen.Left, $screen.Top, 0, 0, $bitmap.Size)
+    $bitmap.Save($screenShotPath, [System.Drawing.Imaging.ImageFormat]::Jpeg)
+}
+
+function WriteScreenshotMac([string]$screenShotPath) {
+    & screencapture -t jpg -x $screenShotPath 
+}
+
+
+[string]$capFilename = "$PSScriptRoot\shot.jpg"
+
+if ($IsWindows) {
+    WriteScreenShotWin $capFilename
+}
+elseif ($IsMacOS) {
+    WriteScreenShotMac $capFilename
+}
+else {
+    throw "Only MacOS and Windows supported"
+    exit
+}
+
+Write-Output "Screenshot saved to: $capFilename"
+
+
+
+
